f23ad62f0ac457f8c862b03b1b27d2d74eeb18bf
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-28 13:09:54 +0700

    Update rephrase.js

--- src/rephrase.js
+++ src/rephrase.js
@@ -1,1 +1,25 @@
+function rephraseComment(comment, sentiment, language) {
+  const phrases = {
+    en: {
+      positive: `That's great! ${comment}`,
+      negative: `Keep it up! Don't let it get you down. ${comment}`
+    },
+    es: {
+      positive: `¡Eso es genial! ${comment}`,
+      negative: `¡Ánimo! No te desanimes. ${comment}`
+    },
+    fr: {
+      positive: `C'est super! ${comment}`,
+      negative: `Ne te laisse pas abattre. ${comment}`
+    },
+    id: {
+      positive: `Itu hebat! ${comment}`,
+      negative: `Tetap semangat! Jangan biarkan hal itu membuatmu down. ${comment}`
+    }
+  };
 
+  const phrasesForLanguage = phrases[language] || phrases['en'];
+  return sentiment === 'positive' ? phrasesForLanguage.positive : phrasesForLanguage.negative;
+}
+
+module.exports = { rephraseComment };
