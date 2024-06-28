80d3f41ad381ee2376d71c118093e9f33cfc88f3
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-28 13:11:19 +0700

    Update preprocess.js

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,1 +1,7 @@
+async function preprocess(comment, embeddings, language) {
+  const tokens = comment.split(' ');
+  const vector = tokens.map(token => embeddings.getVector(token).values);
+  return [].concat(...vector);
+}
 
+module.exports = { preprocess };
