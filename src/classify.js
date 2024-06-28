caf96ecb567ddeff5d666efc37a58779f700ea4e
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-28 13:06:36 +0700

    Update classify.js

--- src/classify.js
+++ src/classify.js
@@ -1,1 +1,20 @@
+const tf = require('@tensorflow/tfjs-node');
+const { preprocess } = require('./preprocess');
+const word2vec = require('word2vec');
 
+async function loadModel(modelPath) {
+  return await tf.loadLayersModel(`file://${modelPath}`);
+}
+
+async function loadEmbeddings(embeddingPath) {
+  return await word2vec.loadModel(embeddingPath);
+}
+
+async function classifyComment(model, comment, embeddings, language) {
+  const processedComment = await preprocess(comment, embeddings, language);
+  const prediction = model.predict(tf.tensor([processedComment]));
+  const sentiment = prediction.dataSync()[0];
+  return sentiment > 0.5 ? 'positive' : 'negative';
+}
+
+module.exports = { loadModel, loadEmbeddings, classifyComment };
