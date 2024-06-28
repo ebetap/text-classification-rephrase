268270670123c2c939f53ddd236d3c9a00459863
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-28 13:10:28 +0700

    Update language.js

--- src/language.js
+++ src/language.js
@@ -1,1 +1,19 @@
+const franc = require('franc');
 
+function detectLanguage(comment) {
+  const langCode = franc(comment);
+  switch (langCode) {
+    case 'eng':
+      return 'en';
+    case 'spa':
+      return 'es';
+    case 'fra':
+      return 'fr';
+    case 'ind':
+      return 'id';
+    default:
+      return 'en';
+  }
+}
+
+module.exports = { detectLanguage };
